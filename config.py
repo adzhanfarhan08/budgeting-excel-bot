2efbb2b018b3522d44b12c52f28e415ba2adaa16
adzhanfarhan08 <adzhanfarhan@gmail.com>
2025-06-16 02:48:43 +0700

    update 5 files

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
-import logging
+import logging, os
 from pathlib import Path
+from dotenv import load_dotenv
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 # Excel File
 EXPORT_DIR = Path("export")
 EXPORT_DIR.mkdir(exist_ok=True)
 EXCEL_FILE = EXPORT_DIR / "budget_data.xlsx"
 
-# TOKEN
-BOT_TOKEN = "8086600951:AAEMGPG_LLqxVW5rJ1p9eZJb6uOC3K03STA"
+if not BOT_TOKEN:
+    raise ValueError("Token bot tidak ditemukan! Pastikan file .env sudah dibuat")
